3d6980648bb8a04f9d7d4a5589bcada617e44584
maerad7 <ds.jeong@smsoft.co.kr>
2020-07-06 17:09:42 +0900

    study

---
+++ C#/17. IndexerTest/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace IndexerTest
+{
+    class MyClass
+    {
+        private const int MAX = 10;
+        private string name;
+
+        private int[] data = new int[MAX];
+
+        public int this[int index]
+        {
+            get
+            {
+                if ( index < 0 || index >= MAX)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                else
+                {
+                    return data[index];
+                }
+            }
+            set
+            {
+                if (!(index < 0 || index >= MAX))
+                {
+                    data[index] = value;
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass cls = new MyClass();
+
+            cls[1] = 1204;
+
+            int i= cls[1];
+
+            Console.WriteLine(i);
+        }
+    }
+}
