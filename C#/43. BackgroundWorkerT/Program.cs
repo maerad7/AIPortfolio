f2cd6feaea09151ad72836bcb34530d7530debb8
maerad7 <ds.jeong@smsoft.co.kr>
2020-07-10 17:19:05 +0900

    study

---
+++ C#/43. BackgroundWorkerT/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace BackgroundWorkerT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Execute();
+            Console.Read();
+        
+        }
+
+        private BackgroundWorker worker;
+        
+        public void Execute()
+        {
+            // 쓰레드풀에서 작업 쓰레드 시작
+            worker = new BackgroundWorker();
+            worker.DoWork += new DoWorkEventHandler(worker_DoWork);
+            worker.RunWorkerAsync();
+        }
+
+        // 작업 쓰레드가 실행할 Task 메서드
+        void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // 긴 처리 가정
+            Console.WriteLine("Long running Task");
+        }
+    }
+}
---
+++ C#/44. TaskClassT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _44._TaskClassT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
